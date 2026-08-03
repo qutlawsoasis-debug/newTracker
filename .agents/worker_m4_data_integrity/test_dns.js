89de35a80716506359d03d50927c559de522f66d
qutlawsoasis-debug <qutlawsoasis@gmail.com>
2026-08-04 01:30:38 +0200

    security: remove internal audit logs and add .agents to .gitignore

--- .agents/worker_m4_data_integrity/test_dns.js
+++
@@ -1,9 +0,0 @@
-const dns = require('dns');
-
-dns.lookup('duajmoeuumbqncoftzpu.supabase.co', (err, address, family) => {
-  console.log('dns.lookup result:', err, address, family);
-});
-
-dns.resolve4('duajmoeuumbqncoftzpu.supabase.co', (err, addresses) => {
-  console.log('dns.resolve4 result:', err, addresses);
-});
